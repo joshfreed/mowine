2e8783c5fb99b515b62626988da44fae368f99b2
Josh Freed <joshfreed3@gmail.com>
2017-01-07 14:16:29 -0500

    Initial Commit

---
+++ mowine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mowine
+//
+//  Created by Josh Freed on 1/7/17.
+//  Copyright © 2017 BleepSmazz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
