069a013c72ce0fcb277fc80e1ea4009b2ff718c6
Josh Freed <josh@jpfreed.com>
2018-12-11 14:40:54 -0500

    Removed pods from the repo

--- Pods/Target Support Files/Pods-mowineTests/Pods-mowineTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mowineTests : NSObject
-@end
-@implementation PodsDummy_Pods_mowineTests
-@end
--- Pods/Target Support Files/Pods-mowineUITests/Pods-mowineUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mowineUITests : NSObject
-@end
-@implementation PodsDummy_Pods_mowineUITests
-@end
