703695a9000378876f41f3d8285339b969caa092
Josh Freed <josh@jpfreed.com>
2020-11-29 15:45:37 -0500

    Started working on the reauthentication view

--- mowine/UI/Views/Auth/EmailLoginButton.swift
+++ mowine/UI/Views/Auth/EmailLoginButton.swift
@@ -9,8 +9,22 @@
 import SwiftUI
 
 struct EmailLoginButton: View {
+    var action: () -> Void = { }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action, label: {
+            Text("Continue with Email")
+                .font(.system(size: 21))
+                .fontWeight(.light)
+                .foregroundColor(Color("Primary"))
+        })
+        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .frame(height: 72)
+        .overlay(
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(Color("Primary"), lineWidth: 1)
+        )
     }
 }
 
