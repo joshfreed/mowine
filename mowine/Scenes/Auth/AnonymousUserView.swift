712020349863c88c9b7719e25c6b27fa84950bd3
Josh Freed <josh@jpfreed.com>
2021-01-03 18:41:13 -0500

    Spruced up the design and copy of the anonymous user screen

--- mowine/Scenes/Auth/AnonymousUserView.swift
+++ mowine/Scenes/Auth/AnonymousUserView.swift
@@ -10,41 +10,36 @@ import SwiftUI
 
 struct AnonymousUserView: View {
     var action: (MyAccountSheet) -> Void
+    let buttonHeight: CGFloat = 50
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Spacer()
-            
-            Text("Go mo' out of mo' wine with a free account")
-                .font(.title)
+        VStack(spacing: 0) {
+            Text("Get mo' out of mo' wine with a free account")
+                .fontWeight(.black)
                 .multilineTextAlignment(.center)
-            
+                .fixedSize(horizontal: false, vertical: true)
+                .font(.system(size: 37))
+                .foregroundColor(.white)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding(.top, 100)
+                .padding(.bottom, 56)
+                .background(Color("Primary"))
+
             Spacer()
             
-            VStack(alignment: .leading) {
-                Text("You'll always know the best wine to bring")
-                    .font(.title2)
+            VStack(spacing: 40) {
+                CoolCopyView(
+                    image: "Red Wine Button",
+                    headline: "Connect with your friends so they can see the wines you love and hate."
+                )
                 
-                Text("Find your friends and bring them their favorite wines.")
-                    .font(.callout)
+                CoolCopyView(
+                    image: "Bubbly Button",
+                    headline: "Sync your data across all your Apple devices."
+                )
             }
-            
-            VStack(alignment: .leading) {
-                Text("Never receive a bad wine again")
-                    .font(.title2)
-                
-                Text("Your friends can discover your favorite wines.")
-                    .font(.callout)
-            }
-            
-            VStack(alignment: .leading) {
-                Text("Your wines are safe in the cloud")
-                    .font(.title2)
-                
-                Text("Backup and access your wines on any Apple device.")
-                    .font(.callout)
-            }
-            
+            .padding([.leading, .trailing])
+                        
             Spacer()
             
             HStack {
@@ -52,20 +47,42 @@ struct AnonymousUserView: View {
                     action: { action(.logIn) },
                     title: "Log In",
                     isLoading: .constant(false),
-                    height: 60
+                    height: buttonHeight
                 )
                 
                 PrimaryButton(
                     action: { action(.signUp) },
                     title: "Sign Up",
                     isLoading: .constant(false),
-                    height: 60
+                    height: buttonHeight
                 )
             }
+            .padding([.leading, .trailing])
             
             Spacer()
         }
-        .padding()        
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct CoolCopyView: View {
+    let image: String
+    let headline: String
+    
+    var body: some View {
+        VStack(spacing: 4) {
+            Image(image)
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 60, height: 60)
+                .foregroundColor(Color("Dark Gray"))
+            
+            Text(headline)
+                .fixedSize(horizontal: false, vertical: true)
+                .font(.system(size: 21))
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
